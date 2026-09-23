c00a7c553487d199f14089e2297be569bd415ded
samuelmolineroalvarez-cloud <samuelmolineroalvarez@gmail.com>
2026-09-23 14:56:27 +0300

    Change weather API endpoint and fix temperature access
    Updated API endpoint and adjusted data extraction for temperature.

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const API_KEY = "f023fa58fdb0065d1e7b17d248d169ab";
 
 const url =
-    `https://api.openweathermap.org/data/2.5/weather?q=Lahti&units=metric&appid=${API_KEY}`;
+    `https://api.openweathermap.org/data/4.0/onecall/current?lat=60.9827&lon=25.6615&units=metric&appid=${API_KEY}`;
 
 fetch(url)
     .then(response => response.json())
     .then(data => {
-        const temperature = data.main.temp;
+        const temperature = data.data[0].temp;
 
         document.getElementById("temperature").textContent =
             `Temperature: ${temperature} °C`;
     })
     .catch(error => {
-        document.getElementById("temperature").textContent =
-            "Could not obtain temperature data.";
-
         console.error(error);
+        document.getElementById("temperature").textContent =
+            "Error obtaining temperature";
     });
